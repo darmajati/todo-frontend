afef8d20e0346490e4a3027b4d4060b0b42b83bc
Pranowo Darmajati <darmajati45@gmail.com>
2023-08-15 10:47:50 +0700

    resolve Link from home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
 import TodoList from "../components/TodoList"
 
