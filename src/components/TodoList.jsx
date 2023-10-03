714f74745bed03a19cfaa8b0579c3b348e347daf
Pranowo Darmajati <darmajati45@gmail.com>
2023-10-03 17:00:00 +0700

    add loading feature for login and Todo List content

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import TodoItem from "./TodoItem";
 import { fetchTodos } from "../redux/actions/todoActions";
 import { Table, Container, Button } from "react-bootstrap";
 import { deleteTodo } from "../redux/actions/todoActions";
 import AddTodo from "./AddTodo";
-import { Link } from "react-router-dom"
+import { BarLoader } from "react-spinners";
 
 const TodoList = () => {
   const todos = useSelector((state) => state.todo.todos);
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true); // Set initially to true
 
   useEffect(() => {
-    dispatch(fetchTodos());
+    // Fetch todos and set isLoading to false when data is loaded
+    dispatch(fetchTodos())
+      .then(() => {
+        setIsLoading(false);
+      });
   }, [dispatch]);
 
   const handleDelete = async (id) => {
@@ -25,24 +30,30 @@ const TodoList = () => {
       <Container>
         <h2 style={{ textAlign: "center", margin: "20px 0" }}>Todo List</h2>
         <AddTodo />
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Judul</th>
-              <th>Deskripsi</th>
-              <th>Status</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            {todos.map((todo, index) => (
-              <TodoItem key={todo.id} todo={todo} index={index} onDelete={() => handleDelete(todo.id)} />
-            ))}
-          </tbody>
-        </Table>
-
-        <Link to="/"><Button variant="success"> &lt; Kembali</Button></Link>
+        {isLoading ? (
+          // Show loading spinner while data is being fetched
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "200px" }}>
+            <BarLoader color="#36d7b7" width={800} loading={isLoading} />
+          </div>
+        ) : (
+          // Show the TodoList when data is loaded
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Judul</th>
+                <th>Deskripsi</th>
+                <th>Status</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              {todos.map((todo, index) => (
+                <TodoItem key={todo.id} todo={todo} index={index} onDelete={() => handleDelete(todo.id)} />
+              ))}
+            </tbody>
+          </Table>
+        )}
       </Container>
     </div>
   );
